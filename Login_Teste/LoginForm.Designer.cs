86bc20106aafb1c58190712fbe2f7dd0af6a9f7f
Ruan <darkcraft399@gmail.com>
2024-08-28 20:32:22 -0300

    Add Project

---
+++ Login_Teste/LoginForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Login_Teste
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            panel1 = new Panel();
+            campoSenha = new TextBox();
+            campoUsuario = new TextBox();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(46, 140);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 22);
+            button1.TabIndex = 0;
+            button1.Text = "Entrar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(70, 201);
+            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.Name = "button2";
+            button2.Size = new Size(125, 22);
+            button2.TabIndex = 1;
+            button2.Text = "Cadastre-se";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(campoSenha);
+            panel1.Controls.Add(campoUsuario);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(21, 11);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(257, 244);
+            panel1.TabIndex = 2;
+            // 
+            // campoSenha
+            // 
+            campoSenha.Location = new Point(46, 80);
+            campoSenha.Margin = new Padding(3, 2, 3, 2);
+            campoSenha.Name = "campoSenha";
+            campoSenha.PasswordChar = '*';
+            campoSenha.Size = new Size(174, 23);
+            campoSenha.TabIndex = 3;
+            // 
+            // campoUsuario
+            // 
+            campoUsuario.Location = new Point(46, 35);
+            campoUsuario.Margin = new Padding(3, 2, 3, 2);
+            campoUsuario.Name = "campoUsuario";
+            campoUsuario.Size = new Size(174, 23);
+            campoUsuario.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(305, 280);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Panel panel1;
+        private TextBox campoSenha;
+        private TextBox campoUsuario;
+    }
+}
